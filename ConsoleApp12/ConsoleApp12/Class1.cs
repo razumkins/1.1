78480868011deccebe17f4d8e6827b004167483b
Haritonova Olga <haritonova.olyakpc2002020@gmail.com>
2022-05-26 14:16:24 +0300

    123

---
+++ ConsoleApp12/ConsoleApp12/Class1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace дз_рпм
+{
+    class drob
+    {
+        int Chis;
+        int Znam;
+        internal bool Znak;
+
+        public drob(int chis1, int znam1)
+        {
+            Chis = chis1;
+            Znam = znam1;
+        }
+        public drob(int chislo)
+        {
+            Chis = chislo;
+            Znam = 1;
+        }
+        public drob(int celoe, int chis2, int znam2)
+        {
+            Chis = celoe * znam2 + chis2;
+            Znam = znam2;
+        }
+        public double ToDouble()
+        {
+            return (double)(Chis) / Znam;
+        }
+        public static drob operator *(drob a, drob b)
+        {
+            return new drob(a.Chis * b.Chis, a.Znam * b.Znam);
+        }
+        public bool MyProperty
+        {
+            get
+            {
+                bool a = Chis >= 0;
+                bool b = Znam >= 0;
+                return a == b;
+            }
+        }
+    }
+}
---
+++ ConsoleApp12/ConsoleApp12/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace дз_рпм
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            drob d1 = new drob(2, 3);
+            drob d2 = new drob(2, 3, 4);
+            drob d3 = new drob(-2);
+            Console.WriteLine(d1.ToDouble());
+            Console.WriteLine((d1 * d2).ToDouble());
+            Console.WriteLine((d1 * d3).Znak);
+            Console.ReadKey();
+        }
+    }
+}
